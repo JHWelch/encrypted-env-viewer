3d7af32c1e4677a2e057bc003802696b28707dac
Jordan Welch <jordan.welch@complyant.co>
2023-09-05 12:58:29 -0500

    remove comments

--- src/github_content.js
+++ src/github_content.js
@@ -48,13 +48,10 @@ function processEncryptedFileDivs(a, b) {
 }
 
 function addLocationObserver(callback) {
-    // Options for the observer (which mutations to observe)
     const config = { attributes: false, childList: true, subtree: false }
 
-    // Create an observer instance linked to the callback function
     const observer = new MutationObserver(callback)
 
-    // Start observing the target node for configured mutations
     observer.observe(document.body, config)
 }
 
