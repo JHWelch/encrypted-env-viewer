e5a6402a01a8ef1fc7bf9cadd7f41a15ebfb7473
Jordan Welch <jordanhwelch@gmail.com>
2023-09-07 18:59:51 -0500

    Test diff

--- src/diff.js
+++ src/diff.js
@@ -11,7 +11,7 @@ const diff = (left, right) => Diff.createPatch('patch', left, right);
 
 export const diffHtml = (left, right) => {
   const html = Diff2html.html(diff(left, right), diff2HtmlConfig);
-  const doc = new DOMParser().parseFromString(html, 'text/html');
+  const doc = new window.DOMParser().parseFromString(html, 'text/html');
   const header = doc.querySelector('.d2h-file-header');
   header.parentNode.removeChild(header);
 
---
+++ test/diff.spec.js
@@ -0,0 +1,27 @@
+import * as Diff2html from 'diff2html';
+import fs from 'fs';
+import { diffHtml } from '../src/diff';
+
+describe('diffHtml', () => {
+  let html
+
+  before((done) => {
+    fs.readFile('./test/fixtures/diff.html', 'utf8', (err, data) => {
+      if (err) throw err;
+      html = data;
+      done();
+    });
+  });
+
+  beforeEach(() => {
+    sinon.replace(Diff2html, 'html', sinon.fake.returns(html));
+  });
+
+  it('returns the parsed document', () => {
+    const doc = diffHtml('_', '_');
+
+    expect(doc).to.exist;
+    expect(doc.querySelector('.d2h-wrapper')).to.exist;
+    expect(doc.querySelector('.d2h-file-header')).to.not.exist;
+  });
+});
