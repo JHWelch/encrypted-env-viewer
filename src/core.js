096a7dfc9e6f55c8c40f0b08d4b129af22e54ce1
Jordan Welch <jordanhwelch@gmail.com>
2023-09-07 22:14:27 -0500

    Move env_viewer

--- src/core.js
+++ src/core.js
@@ -1,28 +1,5 @@
-import { decryptEnv } from './decrypt';
-import { diffHtml } from './diff';
-import dom from './dom';
+import { initEnvViewer } from './env_viewer';
 import { sleep } from './helpers';
-
-const handleEncryptedFile = (fileDiv) => {
-  const left = dom.fileContents(fileDiv, 'left');
-  const right = dom.fileContents(fileDiv, 'right');
-
-  if (!left || !right) {
-    alert('Could not find file contents');
-    return;
-  }
-
-  dom.addDecryptButton(fileDiv, () => {
-    const key = prompt('Enter encryption key');
-
-    Promise.all([
-      decryptEnv(left, key),
-      decryptEnv(right, key),
-    ]).then(([leftDecrypted, rightDecrypted]) => {
-      dom.addNewDiff(fileDiv, diffHtml(leftDecrypted, rightDecrypted));
-    });
-  });
-};
 
 export const addLocationObserver = (callback) => {
   const config = { attributes: false, childList: true, subtree: false };
@@ -38,6 +15,3 @@ export const observerCallback = () => {
     sleep(1000).then(initEnvViewer);
   }
 };
-
-export const initEnvViewer = () => dom.encryptedFiles()
-  .forEach(handleEncryptedFile);
---
+++ src/env_viewer.js
@@ -0,0 +1,27 @@
+import { decryptEnv } from './decrypt';
+import { diffHtml } from './diff';
+import dom from './dom';
+
+export const initEnvViewer = () => dom.encryptedFiles()
+  .forEach(handleEncryptedFile);
+
+const handleEncryptedFile = (fileDiv) => {
+  const left = dom.fileContents(fileDiv, 'left');
+  const right = dom.fileContents(fileDiv, 'right');
+
+  if (!left || !right) {
+    alert('Could not find file contents');
+    return;
+  }
+
+  dom.addDecryptButton(fileDiv, () => {
+    const key = prompt('Enter encryption key');
+
+    Promise.all([
+      decryptEnv(left, key),
+      decryptEnv(right, key),
+    ]).then(([leftDecrypted, rightDecrypted]) => {
+      dom.addNewDiff(fileDiv, diffHtml(leftDecrypted, rightDecrypted));
+    });
+  });
+};
