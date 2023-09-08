6af14b5df91760e51b7293b03c8daaa1b8ed45c1
Jordan Welch <jordanhwelch@gmail.com>
2023-09-07 19:02:37 -0500

    split into two tests

--- test/diff.spec.js
+++ test/diff.spec.js
@@ -17,11 +17,18 @@ describe('diffHtml', () => {
     sinon.replace(Diff2html, 'html', sinon.fake.returns(html));
   });
 
+  afterEach(() => sinon.restore());
+
   it('returns the parsed document', () => {
     const doc = diffHtml('_', '_');
 
     expect(doc).to.exist;
     expect(doc.querySelector('.d2h-wrapper')).to.exist;
+  });
+
+  it('removes the file-header', () => {
+    const doc = diffHtml('_', '_');
+
     expect(doc.querySelector('.d2h-file-header')).to.not.exist;
   });
 });
