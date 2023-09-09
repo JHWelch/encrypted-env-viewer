da83171f476d761b1bf64b9a2ab8b896226bc143
Jordan Welch <jordanhwelch@gmail.com>
2023-09-08 19:54:41 -0500

    match to actual PR page

--- src/core.js
+++ src/core.js
@@ -1,6 +1,9 @@
 import { initEnvViewer } from './env_viewer';
 import { sleep } from './helpers';
 
+const PR_URL_REGEX =
+  /https:\/\/github\.com\/[A-Za-z0-9_.-]+\/[A-Za-z0-9_.-]+\/pull\/\d+\/files/;
+
 export const addLocationObserver = (callback) => {
   const observer = new MutationObserver(callback);
 
@@ -12,7 +15,7 @@ export const addLocationObserver = (callback) => {
 };
 
 export const observerCallback = async () => {
-  if (window.location.href.startsWith('https://github.com')) {
+  if (window.location.href.match(PR_URL_REGEX)) {
     // Has to wait for page load
     await sleep(1000);
 
