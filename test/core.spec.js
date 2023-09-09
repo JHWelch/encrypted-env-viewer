10f855e5d064a3a7f2d067c23585a4346f86304b
Jordan Welch <jordanhwelch@gmail.com>
2023-09-08 23:01:25 -0500

    Move env-viewer spec

--- test/core.spec.js
+++ test/core.spec.js
@@ -1,35 +1,9 @@
 import { observerCallback } from '../src/core';
-import { loadFixture } from './support/helpers';
 import * as helpers from '../src/helpers';
 import * as env_viewer from '../src/env_viewer';
 
 var chai = require('chai');
 chai.use(require('sinon-chai'));
-
-describe('initEnvViewer', () => {
-  let html;
-
-  before(() => {
-    html = loadFixture('github_pr.html');
-  });
-
-  beforeEach(() => {
-    global.document = new window.DOMParser().parseFromString(html, 'text/html');
-  });
-
-  it('adds a button to the page with correct attributes', () => {
-    env_viewer.initEnvViewer();
-
-    const button = document.querySelector('[data-test="decrypt-env"]');
-
-    expect(button).to.exist;
-    expect(button.classList.contains('btn')).to.be.true;
-    expect(button.classList.contains('btn-sm')).to.be.true;
-    expect(button.classList.contains('btn-secondary')).to.be.true;
-    expect(button.classList.contains('ml-2')).to.be.true;
-    expect(button.innerHTML).to.equal('Decrypt');
-  });
-});
 
 describe('observerCallback', () => {
   let originalWindow;
---
+++ test/env_viewer.spec.js
@@ -0,0 +1,27 @@
+import { initEnvViewer } from '../src/env_viewer';
+import { loadFixture } from './support/helpers';
+
+describe('initEnvViewer', () => {
+  let html;
+
+  before(() => {
+    html = loadFixture('github_pr.html');
+  });
+
+  beforeEach(() => {
+    global.document = new window.DOMParser().parseFromString(html, 'text/html');
+  });
+
+  it('adds a button to the page with correct attributes', () => {
+    initEnvViewer();
+
+    const button = document.querySelector('[data-test="decrypt-env"]');
+
+    expect(button).to.exist;
+    expect(button.classList.contains('btn')).to.be.true;
+    expect(button.classList.contains('btn-sm')).to.be.true;
+    expect(button.classList.contains('btn-secondary')).to.be.true;
+    expect(button.classList.contains('ml-2')).to.be.true;
+    expect(button.innerHTML).to.equal('Decrypt');
+  });
+});
